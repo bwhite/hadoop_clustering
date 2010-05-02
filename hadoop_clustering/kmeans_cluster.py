213aed8bbdfee36307a6aa1e04025ac31e505711
brandyn <brandyn@brandynd.cs.umd.edu>
2010-05-02 11:03:49 -0400

    Updated

--- hadoop_clustering/kmeans_cluster.py
+++ hadoop_clustering/kmeans_cluster.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import time
 import cPickle as pickle
 
 import numpy as np
@@ -14,10 +15,11 @@ class Mapper(object):
             self.clusters = pickle.load(fp)
         self.nn = __import__(os.environ['NN_MODULE'],
                              fromlist=['nn']).nn
+        self.start_time = time.time()
         
     def map(self, key, feat):
         feat = np.fromstring(feat, dtype=np.float32)
-        nearest_ind = self.nn(feat, self.clusters)
+        nearest_ind = self.nn(feat, self.clusters)[0]
         # Expand the array by 1 and use it to normalize later
         feat = np.resize(feat, feat.shape[0] + 1)
         feat[-1] = 1
@@ -25,25 +27,31 @@ class Mapper(object):
             self.out_sums[nearest_ind] += feat
         except KeyError:
             self.out_sums[nearest_ind] = feat
-        yield
 
     def close(self):
         for nearest_ind, feat in self.out_sums.iteritems():
             yield nearest_ind, feat.tostring()
+        hadoopy.counter('kmeans_cluster','run_time', int(time.time() - self.start_time))
 
+class Reducer(object):
+    def __init__(self):
+        self.start_time = time.time()
 
-def reducer(key, values):
-    cur_cluster_sum = None
-    for vec in values:
-        vec = np.fromstring(vec, dtype=np.float32)
-        try:
-            cur_cluster_sum += vec
-        except TypeError:
-            cur_cluster_sum = vec
-    center = cur_cluster_sum[0:-1] / cur_cluster_sum[-1]
-    yield key, center.tostring()
+    def reduce(self, key, values):
+        cur_cluster_sum = None
+        for vec in values:
+            vec = np.fromstring(vec, dtype=np.float32)
+            try:
+                cur_cluster_sum += vec
+            except TypeError:
+                cur_cluster_sum = vec
+        center = cur_cluster_sum[0:-1] / cur_cluster_sum[-1]
+        yield key, center.tostring()
+    
+    def close(self):
+        hadoopy.counter('kmeans_cluster','run_time', int(time.time() - self.start_time))
 
 
 if __name__ == "__main__":
-    if hadoopy.run(Mapper, reducer):
+    if hadoopy.run(Mapper, Reducer):
         hadoopy.print_doc_quit(__doc__)
